a36b38a20742141b417acad19faf38134ae1c8c9
Lykhosherstyy <evgeniy.arximed@gmail.com>
2018-07-04 21:00:14 +0300

    fixes

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
-
-import { contacts } from './contacts.js';
-import { filter } from './filter.js';
-import { view } from './view.js';
-
-export default combineReducers({ contacts, form, filter, view });
